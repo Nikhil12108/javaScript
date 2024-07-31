16b807d40349ba15ebca30d5f6f3fd3bd9083cb7
Nikhil Kumar <nikhilkumar84157@gmail.com>
2024-07-31 16:35:23 +0530

    data types done

---
+++ baiscs/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // run all file in latest versions of javascript according to new standards
+
+//alert(3 + 3) // we are using nodejs not browser so this works in browser
+
+let name = "Nikhil"; // string
+let age = 23; // number called int also have bigInt
+let isLoggedIn = true; // boolean
+let state; // undefined
+let phoneNumber = null; // standalone datatype represent absence of a value but not undefined;
+
+//symbol // used to represent uniqueness
+
+//object // will be discussed later
+
+// type of undefined is undefined but type of null is object, don't know why it is what it is
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof state);
+console.log(typeof phoneNumber);
+console.log(typeof isLoggedIn);
+
