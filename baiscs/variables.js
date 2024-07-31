008b3c81c2e2ff3645e65135584011246fa95d12
Nikhil Kumar <nikhilkumar84157@gmail.com>
2024-07-31 16:11:12 +0530

    variables completed

---
+++ baiscs/variables.js
@@ -0,0 +1,8 @@
+const account_id = 123; // immutable
+let account_email = "abc@gmail.com"; //mutable
+var account_password = "*##**/*-";//mutable
+account_city = "jaipur"; // worst way to declare a variable don't do this
+
+let accountState;
+
+console.table([account_id, account_email, account_password, account_city, accountState]) // print in a table format with indexes
